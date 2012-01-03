68a2816f5a36416298efcb23fd9483ec14260f95
Julien Lamy <lamy@unistra.fr>
2012-01-03 18:33:06 +0100

    Database : add _dataset_to_document

--- database.cpp
+++ database.cpp
@@ -7,6 +7,7 @@
 
 #include <boost/lexical_cast.hpp>
 #include <gdcmAnonymizer.h>
+#include <gdcmAttribute.h>
 #include <gdcmDataSet.h>
 #include <gdcmFile.h>
 #include <gdcmFileMetaInformation.h>
@@ -64,6 +65,24 @@ void
 Database::
 insert_dataset(gdcm::DataSet const & dataset)
 {
+    gdcm::UIComp sop_instance_uid;
+    {
+        gdcm::DataElement const & de = 
+            dataset.GetDataElement(gdcm::Tag(0x0008,0x0018));
+        gdcm::Attribute<0x0008,0x0018> at;
+        at.SetFromDataElement(de);
+        sop_instance_uid = at.GetValue().Trim();
+    }
+    
+    if(!this->_connection.findOne(this->_db_name+".documents", QUERY("_original_sop_instance_uid" << sop_instance_uid)).isEmpty())
+    {
+        throw std::runtime_error("Cannot insert dataset : already in database");
+    }
+        
+    mongo::BSONObj document = BSON("_original_sop_instance_uid" << sop_instance_uid);
+    
+    this->_dataset_to_document(dataset, document);
+    this->_connection.insert(this->_db_name+".documents", document);
 }
 
 mongo::auto_ptr<mongo::DBClientCursor> 
@@ -185,5 +204,332 @@ Database
         gdcm::LOComp value(subject);
         de.SetByteValue(value, value.length());
         dataset.Insert(de);
+    }
+}
+
+// GDCM forces an even-length string, we don't want this
+template<gdcm::VR::VRType VVR>
+struct VRToType;
+
+template<> struct VRToType<gdcm::VR::AE> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::AS> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::CS> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::DS> { typedef double Type; };
+template<> struct VRToType<gdcm::VR::DA> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::DT> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::FL> { typedef float Type; };
+template<> struct VRToType<gdcm::VR::FD> { typedef double Type; };
+template<> struct VRToType<gdcm::VR::IS> { typedef int32_t Type; };
+template<> struct VRToType<gdcm::VR::LO> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::LT> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::PN> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::SH> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::SL> { typedef int32_t Type; };
+template<> struct VRToType<gdcm::VR::SS> { typedef int16_t Type; };
+template<> struct VRToType<gdcm::VR::ST> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::TM> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::UI> { typedef std::string Type; };
+template<> struct VRToType<gdcm::VR::UL> { typedef uint32_t Type; };
+template<> struct VRToType<gdcm::VR::US> { typedef uint16_t Type; };
+template<> struct VRToType<gdcm::VR::UT> { typedef std::string Type; };
+
+template<typename T>
+T parse_ascii(gdcm::DataElement const & data_element, unsigned long index, bool is_padding_significant)
+{
+    char const * const data = data_element.GetByteValue()->GetPointer();
+    gdcm::VL const length = data_element.GetVL();
+
+    char const * begin = data;
+    for(unsigned long i=0; i<index; ++i)
+    {
+        begin = std::find(begin, data+length, '\\');
+        ++begin;
+    }
+    char const * end = std::find(begin, data+length, '\\');
+
+    if(!is_padding_significant)
+    {
+        while(*begin == ' ')
+        {
+            ++begin;
+        }
+
+        if(end == data+length)
+        {
+            --end;
+        }
+        while(*end == ' ' || *end == '\\' || *end == '\0')
+        {
+            --end;
+        }
+        ++end;
+    }
+
+    return T(std::string(begin, end));
+}
+
+template<typename T>
+T parse_binary(gdcm::DataElement const & data_element, unsigned long index=0)
+{
+    T const * begin = reinterpret_cast<T const *>(data_element.GetByteValue()->GetPointer());
+    return *(begin+index);
+}
+
+template<gdcm::VR::VRType VVR>
+typename VRToType<VVR>::Type parse(gdcm::DataElement const & data_element, unsigned long index=0);
+
+template<>
+VRToType<gdcm::VR::AE>::Type
+parse<gdcm::VR::AE>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::AE>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::AS>::Type
+parse<gdcm::VR::AS>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::AS>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::CS>::Type
+parse<gdcm::VR::CS>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::CS>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::DA>::Type
+parse<gdcm::VR::DA>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::DA>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::DT>::Type
+parse<gdcm::VR::DT>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::DT>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::DS>::Type
+parse<gdcm::VR::DS>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    std::string const value = parse_ascii<std::string>(data_element, index, false);
+    std::istringstream in(value);
+    in.imbue(std::locale("C"));
+    VRToType<gdcm::VR::DS>::Type result;
+    in >> result;
+    return result;
+}
+
+template<>
+VRToType<gdcm::VR::FL>::Type
+parse<gdcm::VR::FL>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_binary<VRToType<gdcm::VR::FL>::Type>(data_element, index);
+}
+
+template<>
+VRToType<gdcm::VR::FD>::Type
+parse<gdcm::VR::FD>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_binary<VRToType<gdcm::VR::FD>::Type>(data_element, index);
+}
+
+template<>
+VRToType<gdcm::VR::IS>::Type
+parse<gdcm::VR::IS>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    std::string const value = parse_ascii<std::string>(data_element, index, false);
+    std::istringstream in(value);
+    in.imbue(std::locale("C"));
+    VRToType<gdcm::VR::IS>::Type result;
+    in >> result;
+    return result;
+}
+
+template<>
+VRToType<gdcm::VR::LO>::Type
+parse<gdcm::VR::LO>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::LO>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::LT>::Type
+parse<gdcm::VR::LT>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::LT>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::PN>::Type
+parse<gdcm::VR::PN>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::PN>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::SH>::Type
+parse<gdcm::VR::SH>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::SH>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::SL>::Type
+parse<gdcm::VR::SL>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_binary<VRToType<gdcm::VR::SL>::Type>(data_element, index);
+}
+
+template<>
+VRToType<gdcm::VR::SS>::Type
+parse<gdcm::VR::SS>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_binary<VRToType<gdcm::VR::SS>::Type>(data_element, index);
+}
+
+template<>
+VRToType<gdcm::VR::ST>::Type
+parse<gdcm::VR::ST>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::ST>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::TM>::Type
+parse<gdcm::VR::TM>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::TM>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::UI>::Type
+parse<gdcm::VR::UI>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::UI>::Type>(data_element, index, false);
+}
+
+template<>
+VRToType<gdcm::VR::UL>::Type
+parse<gdcm::VR::UL>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_binary<VRToType<gdcm::VR::UL>::Type>(data_element, index);
+}
+
+template<>
+VRToType<gdcm::VR::US>::Type
+parse<gdcm::VR::US>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_binary<VRToType<gdcm::VR::US>::Type>(data_element, index);
+}
+
+template<>
+VRToType<gdcm::VR::UT>::Type
+parse<gdcm::VR::UT>(gdcm::DataElement const & data_element, unsigned long index)
+{
+    return parse_ascii<VRToType<gdcm::VR::UT>::Type>(data_element, index, false);
+}
+
+void parse(gdcm::DataElement const & data_element, mongo::BSONObjBuilder & builder)
+{
+    unsigned long count=0;
+
+    if(data_element.GetVR() & (gdcm::VR::LT | gdcm::VR::ST | gdcm::VR::UT))
+    {
+        // LT, ST and UT may not be multi-valued
+        count=1;
+    }
+    else if(gdcm::VR::IsASCII(data_element.GetVR()))
+    {
+        count = 1+std::count(
+            data_element.GetByteValue()->GetPointer(),
+            data_element.GetByteValue()->GetPointer()+data_element.GetVL(),
+            '\\');
+    }
+    else
+    {
+        count = data_element.GetVL()/data_element.GetVR().GetSize();
+    }
+
+#define VR_CASE(vr) \
+    if(vr == data_element.GetVR()) \
+    { \
+        if(count == 1) \
+        { \
+            builder.append(data_element.GetTag().PrintAsPipeSeparatedString(), \
+                           parse<vr>(data_element)); \
+        } \
+        else \
+        { \
+            mongo::BSONArrayBuilder array_builder; \
+            for(unsigned long i=0; i<count; ++i) \
+            { \
+                array_builder.append(parse<vr>(data_element, i)); \
+            } \
+            builder.append(data_element.GetTag().PrintAsPipeSeparatedString(), \
+                           array_builder.arr()); \
+        }\
+    }
+
+    VR_CASE(gdcm::VR::AE);
+    VR_CASE(gdcm::VR::AS);
+//    VR_CASE(gdcm::VR::AT);
+    VR_CASE(gdcm::VR::CS);
+    VR_CASE(gdcm::VR::DA);
+    VR_CASE(gdcm::VR::DS);
+    VR_CASE(gdcm::VR::DT);
+    VR_CASE(gdcm::VR::FD);
+    VR_CASE(gdcm::VR::FL);
+    VR_CASE(gdcm::VR::IS);
+    VR_CASE(gdcm::VR::LO);
+    VR_CASE(gdcm::VR::LT);
+//    VR_CASE(gdcm::VR::OB);
+//    VR_CASE(gdcm::VR::OF);
+//    VR_CASE(gdcm::VR::OW);
+    VR_CASE(gdcm::VR::PN);
+    VR_CASE(gdcm::VR::SH);
+    VR_CASE(gdcm::VR::SL);
+    // SQ is special case !
+//    VR_CASE(gdcm::VR::SQ);
+    VR_CASE(gdcm::VR::SS);
+    VR_CASE(gdcm::VR::ST);
+    VR_CASE(gdcm::VR::TM);
+    VR_CASE(gdcm::VR::UI);
+    VR_CASE(gdcm::VR::UL);
+//    VR_CASE(gdcm::VR::UN);
+    VR_CASE(gdcm::VR::US);
+    VR_CASE(gdcm::VR::UT);
+
+#undef VR_CASE
+}
+
+void
+Database
+::_dataset_to_document(gdcm::DataSet const & dataset, mongo::BSONObj & document)
+{
+    mongo::BSONObjBuilder builder;
+    
+    for(gdcm::DataSet::ConstIterator it=dataset.Begin();
+        it!=dataset.End(); ++it)
+    {
+        if(it->GetTag() == gdcm::Tag(0x7fe0,0x0010) ||
+           it->GetTag() == gdcm::Tag(0x0042,0x0011))
+        {
+            // Pixel Data, Encapsulated Document
+            continue;
+        }
+        if(it->GetTag().GetGroup()/256==0x60 && it->GetTag().GetElement()==0x3000)
+        {
+            // Overlay Data (60xx,3000)
+            continue;
+        }
+        
+        // Get value
+        parse(*it, builder);
     }
 }
