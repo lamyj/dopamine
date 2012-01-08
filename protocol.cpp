cf7ea2a2085b7161d1b166b317ac2d150355039d
Julien Lamy <lamy@unistra.fr>
2012-01-08 17:21:37 +0100

    Add Protocol class

---
+++ protocol.cpp
@@ -0,0 +1,81 @@
+#include "protocol.h"
+
+#include <stdexcept>
+#include <string>
+
+#include <mongo/client/dbclient.h>
+
+Protocol
+::Protocol(std::string const & id, std::string const & name, std::string const & sponsor)
+{
+    this->set_id(id);
+    this->set_name(name);
+    this->set_sponsor(sponsor);
+}
+
+std::string const &
+Protocol
+::get_id() const
+{
+    return this->_id;
+}
+
+void
+Protocol
+::set_id(std::string const & id)
+{
+    this->_id = id;
+}
+
+std::string const &
+Protocol
+::get_name() const
+{
+    return this->_name;
+}
+
+void
+Protocol
+::set_name(std::string const & name)
+{
+    this->_name = name;
+}
+
+std::string const &
+Protocol
+::get_sponsor() const
+{
+    return this->_sponsor;
+}
+
+void
+Protocol
+::set_sponsor(std::string const & sponsor)
+{
+    this->_sponsor = sponsor;
+}
+
+mongo::BSONObj
+Protocol
+::to_bson() const
+{
+    mongo::BSONObj object(BSON(
+        "id" << this->get_id() << 
+        "name" << this->get_name() << 
+        "sponsor" << this->get_sponsor()));
+    return object;
+}
+
+void
+Protocol
+::from_bson(mongo::BSONObj const & object)
+{
+    if(!object.hasField("id"))
+    {
+        throw std::runtime_error("Cannot set Protocol from BSON : no such field \"id\"");
+    }
+    
+    this->set_id(object.getStringField("id"));
+    this->set_name(object.getStringField("name"));
+    this->set_sponsor(object.getStringField("sponsor"));
+}
