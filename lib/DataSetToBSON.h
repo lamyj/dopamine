4e04d24a04ef3b20174399901ead4bb012e9f8c9
Julien Lamy <lamyj@users.noreply.github.com>
2013-01-24 09:54:15 +0100

    lib/DataSetToBSON : refactor code

--- lib/DataSetToBSON.h
+++ lib/DataSetToBSON.h
@@ -47,10 +47,46 @@ private :
 
     //Order::Type _order;
 
+    /// @brief Convert binary data from a DICOM element to BSON.
+    template<gdcm::VR::VRType VVR>
+    void _to_bson(char const * begin, char const * end,
+                  mongo::BSONArrayBuilder & builder) const;
+
+    /**
+     * @brief Convert binary data from a text DICOM element.
+     *
+     * This is used for AE, AS, CS, DA, DT, LO, LT, PN, SH, ST, TM, UI, UT
+     */
+    void _to_bson_text(char const * begin, char const * end,
+                       mongo::BSONArrayBuilder & builder,
+                       bool trim_left, bool trim_right, std::string const & whitespace,
+                       bool multiple_items, bool use_utf8) const;
+
+    /**
+     * @brief Convert binary data from a DICOM element to BSON binary data.
+     *
+     * This is used for OB, OF, OW, UN
+     */
+    void _to_bson_binary(char const * begin, char const * end,
+                         mongo::BSONArrayBuilder & builder) const;
+
+    /**
+     * @brief Convert binary data from a DICOM element to BSON using a simple
+     * reinterpret_cast.
+     *
+     * This is used for FD, FL, SL, SS, UL, US
+     */
+    template<typename T>
+    void _to_bson_reinterpret_cast(char const * begin, char const * end,
+                                   mongo::BSONArrayBuilder & builder, gdcm::VR const & vr) const;
+
+    // Since _to_bson is specialized and instantiated in _add_element,
+    // this function must be declared after the the specializations.
     void _add_element(gdcm::DataElement const & element,
                       mongo::BSONObjBuilder & builder) const;
-    void _add_element(char const * begin, char const * end, gdcm::VR const & vr,
-                      mongo::BSONArrayBuilder & builder) const;
+//    void _add_element(char const * begin, char const * end, gdcm::VR const & vr,
+//                      mongo::BSONArrayBuilder & builder) const;
+
     unsigned long _get_length(char const * begin, char const * end,
                               gdcm::VR const & vr) const;
 
