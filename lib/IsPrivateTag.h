fb0af03255809411b6b7807854c3dd08c129bfbc
Julien Lamy <lamyj@users.noreply.github.com>
2013-03-02 14:39:25 +0100

    lib : add "IsPrivateTag"

---
+++ lib/IsPrivateTag.h
@@ -0,0 +1,19 @@
+#ifndef _361310be_d429_4f49_9d0d_19bd01316dff
+#define _361310be_d429_4f49_9d0d_19bd01316dff
+
+#include <boost/shared_ptr.hpp>
+#include "Condition.h"
+
+class DcmElement;
+
+class IsPrivateTag : public Condition
+{
+public :
+    typedef boost::shared_ptr<IsPrivateTag> Pointer;
+    static Pointer New();
+
+    IsPrivateTag();
+    virtual bool operator()(DcmElement * element) const;
+};
+
+#endif // _361310be_d429_4f49_9d0d_19bd01316dff
