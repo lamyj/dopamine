7065f093e6fe909450892fe8342ad2015f6ad8f3
Julien Lamy <lamy@unistra.fr>
2013-03-05 13:09:35 +0100

    lib/FindResponseGenerator : use correct match type for multiple values matching

--- lib/FindResponseGenerator.h
+++ lib/FindResponseGenerator.h
@@ -14,6 +14,8 @@
 class FindResponseGenerator
 {
 public :
+    typedef FindResponseGenerator Self;
+
     FindResponseGenerator(DcmDataset /*const*/ & query, // DcmDataset is not const-correct
                           mongo::DBClientConnection & connection,
                           std::string const & db_name);
@@ -47,6 +49,30 @@ private :
     /// @brief Return the DICOM Match Type of an element in BSON form.
     Match::Type _get_match_type(std::string const & vr, 
                                 mongo::BSONElement const & element) const;
+
+    /**
+     * @brief Convert a BSON element from the DICOM query language to the
+     *        MongoDB query language.
+     *
+     * This function must be specialized for each value of Self::Match.
+     */
+    template<Match::Type VType>
+    void _dicom_query_to_mongo_query(
+        std::string const & field, std::string const & vr,
+        mongo::BSONElement const & value,
+        mongo::BSONObjBuilder & builder) const;
+
+    /// @brief Type of DICOM query -> MongoDB query conversion functions.
+    typedef void (Self::*DicomQueryToMongoQuery)(
+        std::string const & field, std::string const & vr,
+        mongo::BSONElement const & value,
+        mongo::BSONObjBuilder & builder) const;
+
+    /**
+     * @brief Return the DICOM query -> MongoDB query conversion function
+     *        corresponding to the specified match type.
+     */
+    DicomQueryToMongoQuery _get_query_conversion(Match::Type const & match_type) const;
 };
 
 #endif // _ee9915a2_a504_4b21_8d43_7938c66c526e
