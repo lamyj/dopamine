c043bcc5364853e3bf029cc65196473de9fd78e1
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-08-19 10:48:10 +0200

    tests/code/ConverterBSON: add test files for classes IsPrivateTag, TagMatch and VRMatch

---
+++ tests/code/ConverterBSON/IsPrivateTag.cpp
@@ -0,0 +1,55 @@
+/*************************************************************************
+ * Research_pacs - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#define BOOST_TEST_MODULE ModuleIsPrivateTag
+#include <boost/test/unit_test.hpp>
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmdata/dctk.h>
+
+#include "ConverterBSON/IsPrivateTag.h"
+#include "core/ExceptionPACS.h"
+
+/*************************** TEST OK 01 *******************************/
+/**
+ * Nominal test case: Private Tag
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_01)
+{
+    auto isprivatetag = IsPrivateTag::New();
+    
+    DcmElement* element = new DcmAttributeTag(DcmTag(0023,1001));
+    BOOST_CHECK_EQUAL((*isprivatetag)(element), true);
+    
+    delete element;
+}
+
+/*************************** TEST OK 02 *******************************/
+/**
+ * Nominal test case: Public Tag
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_02)
+{
+    auto isprivatetag = IsPrivateTag::New();
+    
+    DcmElement* element = new DcmAttributeTag(DcmTag(0010,0010));
+    BOOST_CHECK_EQUAL((*isprivatetag)(element), false);
+    
+    delete element;
+}
+
+/*************************** TEST KO 01 *******************************/
+/**
+ * Error test case: Empty element
+ */
+BOOST_AUTO_TEST_CASE(TEST_KO_01)
+{
+    auto isprivatetag = IsPrivateTag::New();
+    
+    BOOST_REQUIRE_THROW((*isprivatetag)(NULL), research_pacs::ExceptionPACS);
+}
---
+++ tests/code/ConverterBSON/TagMatch.cpp
@@ -0,0 +1,68 @@
+/*************************************************************************
+ * Research_pacs - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#define BOOST_TEST_MODULE ModuleTagMatch
+#include <boost/test/unit_test.hpp>
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmdata/dctk.h>
+
+#include "ConverterBSON/TagMatch.h"
+
+/*************************** TEST OK 01 *******************************/
+/**
+ * Nominal test case: Tag Match
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_01)
+{
+    auto tagmatch = TagMatch::New(DcmTagKey(0x0010, 0x0010));
+    
+    DcmElement* element = new DcmAttributeTag(DcmTag(0x0010, 0x0010));
+    BOOST_CHECK_EQUAL((*tagmatch)(element), true);
+    
+    delete element;
+}
+
+/*************************** TEST OK 02 *******************************/
+/**
+ * Nominal test case: Tag not Match => different Group
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_02)
+{
+    auto tagmatch = TagMatch::New(DcmTagKey(0x0008, 0x0010));
+    
+    DcmElement* element = new DcmAttributeTag(DcmTag(0x0010, 0x0010));
+    BOOST_CHECK_EQUAL((*tagmatch)(element), false);
+    
+    delete element;
+}
+
+/*************************** TEST OK 03 *******************************/
+/**
+ * Nominal test case: Tag not Match => different Element
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_03)
+{
+    auto tagmatch = TagMatch::New(DcmTagKey(0x0010, 0x0020));
+    
+    DcmElement* element = new DcmAttributeTag(DcmTag(0x0010, 0x0010));
+    BOOST_CHECK_EQUAL((*tagmatch)(element), false);
+    
+    delete element;
+}
+
+/*************************** TEST OK 04 *******************************/
+/**
+ * Nominal test case: Tag not Match => tested element is empty
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_04)
+{
+    auto tagmatch = TagMatch::New(DcmTagKey(0x0010, 0x0010));
+    
+    BOOST_CHECK_EQUAL((*tagmatch)(NULL), false);
+}
