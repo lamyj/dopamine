8f1885c63b5704ea72b81a32cc367dc561c0d33b
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-08-19 09:48:22 +0200

    tests/code/ConverterBSON: add AlwaysTrue test file

---
+++ tests/code/ConverterBSON/AlwaysTrue.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+ * Research_pacs - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#define BOOST_TEST_MODULE ModuleAlwaysTrue
+#include <boost/test/unit_test.hpp>
+
+#include "ConverterBSON/AlwaysTrue.h"
+
+/*************************** TEST OK 01 *******************************/
+/**
+ * 
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_01)
+{
+    auto alwaystrue = AlwaysTrue::New();
+    
+    BOOST_CHECK_EQUAL((*alwaystrue)(NULL), true);
+}
