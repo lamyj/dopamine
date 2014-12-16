9809a1ab50b1ab4a4760987f117753cfd4ba7e8d
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-12-16 10:41:42 +0100

    tests/code: delete unused code

--- tests/code/SCP/MoveSCP.cpp
+++ tests/code/SCP/MoveSCP.cpp
@@ -26,6 +26,149 @@
  *     - we assume that DBConnection works correctly
  */
 
+/*************************** Tools functions **************************/
+void
+storeSCPCallback(
+    /* in */
+    void *callbackData,
+    T_DIMSE_StoreProgress *progress,                /* progress state */
+    T_DIMSE_C_StoreRQ *req,                         /* original store request */
+    char *imageFileName, DcmDataset **imageDataSet, /* being received into */
+    /* out */
+    T_DIMSE_C_StoreRSP *rsp,                        /* final store response */
+    DcmDataset **statusDetail)
+{
+    if (progress->state == DIMSE_StoreEnd)
+    {
+       *statusDetail = NULL;    /* no status detail */
+    }
+}
+
+OFCondition
+storeSCP(T_ASC_Association *assoc,
+         T_DIMSE_Message *msg,
+         T_ASC_PresentationContextID presID)
+{
+    T_DIMSE_C_StoreRQ *req = &msg->msg.CStoreRQ;
+
+    DcmFileFormat dcmff;
+    DcmDataset *dset = dcmff.getDataset();
+
+    OFCondition cond = DIMSE_storeProvider(assoc, presID, req, NULL, 0,
+                                           &dset, storeSCPCallback, NULL,
+                                           DIMSE_BLOCKING, 30);
+    if (cond.bad())
+    {
+        std::stringstream stream;
+        stream << "Cannot store dataset: " << cond.text();
+        throw research_pacs::ExceptionPACS(stream.str());
+    }
+
+    return cond;
+}
+
+void
+subOpCallback(void * /*subOpCallbackData*/ ,
+              T_ASC_Network *aNet, T_ASC_Association **subAssoc)
+{
+    if (aNet == NULL) return;
+
+    if (*subAssoc == NULL)
+    {
+        const char* knownAbstractSyntaxes[] =
+        {
+            UID_VerificationSOPClass
+        };
+
+        OFCondition cond = ASC_receiveAssociation(aNet, subAssoc, ASC_DEFAULTMAXPDU);
+        if (cond.good())
+        {
+            const char* transferSyntaxes[] = { NULL, NULL, NULL, NULL };
+            int numTransferSyntaxes;
+            if (gLocalByteOrder == EBO_LittleEndian)  /* defined in dcxfer.h */
+            {
+                transferSyntaxes[0] = UID_LittleEndianExplicitTransferSyntax;
+                transferSyntaxes[1] = UID_BigEndianExplicitTransferSyntax;
+            }
+            else
+            {
+                transferSyntaxes[0] = UID_BigEndianExplicitTransferSyntax;
+                transferSyntaxes[1] = UID_LittleEndianExplicitTransferSyntax;
+            }
+            transferSyntaxes[2] = UID_LittleEndianImplicitTransferSyntax;
+            numTransferSyntaxes = 3;
+
+            /* accept the Verification SOP Class if presented */
+            cond = ASC_acceptContextsWithPreferredTransferSyntaxes(
+                        (*subAssoc)->params, knownAbstractSyntaxes,
+                        DIM_OF(knownAbstractSyntaxes),
+                        transferSyntaxes, numTransferSyntaxes);
+
+            if (cond.good())
+            {
+                /* the array of Storage SOP Class UIDs comes from dcuid.h */
+                cond = ASC_acceptContextsWithPreferredTransferSyntaxes(
+                            (*subAssoc)->params, dcmAllStorageSOPClassUIDs,
+                            numberOfAllDcmStorageSOPClassUIDs,
+                            transferSyntaxes, numTransferSyntaxes);
+            }
+        }
+        if (cond.good()) cond = ASC_acknowledgeAssociation(*subAssoc);
+        if (cond.bad())
+        {
+            cond = ASC_dropAssociation(*subAssoc);
+            cond = ASC_destroyAssociation(subAssoc);
+        }
+    }
+    else
+    {
+        T_DIMSE_Message     msg;
+        T_ASC_PresentationContextID presID;
+
+        if (!ASC_dataWaiting(*subAssoc, 0)) /* just in case */
+            return;
+
+        OFCondition cond = DIMSE_receiveCommand(*subAssoc, DIMSE_BLOCKING, 30,
+                                                &presID, &msg, NULL);
+        if (cond == EC_Normal)
+        {
+            switch (msg.CommandField)
+            {
+            case DIMSE_C_STORE_RQ:
+                cond = storeSCP(*subAssoc, &msg, presID);
+                break;
+            default:
+                cond = DIMSE_BADCOMMANDTYPE;
+                break;
+            }
+        }
+        /* clean up on association termination */
+        if (cond == DUL_PEERREQUESTEDRELEASE)
+        {
+            cond = ASC_acknowledgeRelease(*subAssoc);
+            cond = ASC_dropSCPAssociation(*subAssoc);
+            cond = ASC_destroyAssociation(subAssoc);
+            return;
+        }
+        else if (cond == DUL_PEERABORTEDASSOCIATION)
+        {
+        }
+        else if (cond != EC_Normal)
+        {
+            std::stringstream stream;
+            stream << "DIMSE failure (aborting sub-association): " << cond.text();
+            cond = ASC_abortAssociation(*subAssoc);
+            throw research_pacs::ExceptionPACS(stream.str());
+        }
+
+        if (cond != EC_Normal)
+        {
+            cond = ASC_dropAssociation(*subAssoc);
+            cond = ASC_destroyAssociation(subAssoc);
+        }
+    }
+}
+
 /*************************** TEST OK 01 *******************************/
 /**
  * Nominal test case: Constructor
@@ -92,181 +235,6 @@ struct TestDataOK02
         terminateNetwork();
     }
 };
-
-struct StoreCallbackData
-{
-  char* imageFileName;
-  DcmFileFormat* dcmff;
-  T_ASC_Association* assoc;
-};
-
-void
-storeSCPCallback(
-    /* in */
-    void *callbackData,
-    T_DIMSE_StoreProgress *progress,    /* progress state */
-    T_DIMSE_C_StoreRQ *req,             /* original store request */
-    char *imageFileName, DcmDataset **imageDataSet, /* being received into */
-    /* out */
-    T_DIMSE_C_StoreRSP *rsp,            /* final store response */
-    DcmDataset **statusDetail)
-{
-    if (progress->state == DIMSE_StoreEnd)
-    {
-       *statusDetail = NULL;    /* no status detail */
-    }
-}
-
-OFCondition storeSCP(
-  T_ASC_Association *assoc,
-  T_DIMSE_Message *msg,
-  T_ASC_PresentationContextID presID)
-{
-    OFCondition cond = EC_Normal;
-    T_DIMSE_C_StoreRQ *req;
-    char imageFileName[2048];
-
-    req = &msg->msg.CStoreRQ;
-
-    sprintf(imageFileName, "%s.%s",
-        dcmSOPClassUIDToModality(req->AffectedSOPClassUID),
-        req->AffectedSOPInstanceUID);
-
-    StoreCallbackData callbackData;
-    callbackData.assoc = assoc;
-    callbackData.imageFileName = imageFileName;
-    DcmFileFormat dcmff;
-    callbackData.dcmff = &dcmff;
-
-    // store SourceApplicationEntityTitle in metaheader
-    if (assoc && assoc->params)
-    {
-      const char *aet = assoc->params->DULparams.callingAPTitle;
-      if (aet) dcmff.getMetaInfo()->putAndInsertString(DCM_SourceApplicationEntityTitle, aet);
-    }
-
-    DcmDataset *dset = dcmff.getDataset();
-
-    cond = DIMSE_storeProvider(assoc, presID, req, NULL, 0,
-                               &dset, storeSCPCallback, (void*)&callbackData,
-                               DIMSE_BLOCKING, 30);
-
-    if (cond.bad())
-    {
-        std::stringstream stream;
-        stream << "Cannot store dataset: " << cond.text();
-        throw research_pacs::ExceptionPACS(stream.str());
-    }
-
-    return cond;
-}
-
-void
-subOpCallback(void * /*subOpCallbackData*/ ,
-        T_ASC_Network *aNet, T_ASC_Association **subAssoc)
-{
-    if (aNet == NULL) return;   /* help no net ! */
-
-    if (*subAssoc == NULL) {
-        /* negotiate association */
-        const char* knownAbstractSyntaxes[] = {
-            UID_VerificationSOPClass
-        };
-        const char* transferSyntaxes[] = { NULL, NULL, NULL, NULL };
-        int numTransferSyntaxes;
-
-        OFCondition cond = ASC_receiveAssociation(aNet, subAssoc, ASC_DEFAULTMAXPDU);
-        if (cond.good())
-        {
-            /* We prefer explicit transfer syntaxes.
-             * If we are running on a Little Endian machine we prefer
-             * LittleEndianExplicitTransferSyntax to BigEndianTransferSyntax.
-             */
-            if (gLocalByteOrder == EBO_LittleEndian)  /* defined in dcxfer.h */
-            {
-              transferSyntaxes[0] = UID_LittleEndianExplicitTransferSyntax;
-              transferSyntaxes[1] = UID_BigEndianExplicitTransferSyntax;
-            } else {
-              transferSyntaxes[0] = UID_BigEndianExplicitTransferSyntax;
-              transferSyntaxes[1] = UID_LittleEndianExplicitTransferSyntax;
-            }
-            transferSyntaxes[2] = UID_LittleEndianImplicitTransferSyntax;
-            numTransferSyntaxes = 3;
-
-
-            /* accept the Verification SOP Class if presented */
-            cond = ASC_acceptContextsWithPreferredTransferSyntaxes(
-                (*subAssoc)->params,
-                knownAbstractSyntaxes, DIM_OF(knownAbstractSyntaxes),
-                transferSyntaxes, numTransferSyntaxes);
-
-            if (cond.good())
-            {
-                /* the array of Storage SOP Class UIDs comes from dcuid.h */
-                cond = ASC_acceptContextsWithPreferredTransferSyntaxes(
-                    (*subAssoc)->params,
-                    dcmAllStorageSOPClassUIDs, numberOfAllDcmStorageSOPClassUIDs,
-                    transferSyntaxes, numTransferSyntaxes);
-            }
-        }
-        if (cond.good()) cond = ASC_acknowledgeAssociation(*subAssoc);
-        if (cond.bad()) {
-            ASC_dropAssociation(*subAssoc);
-            ASC_destroyAssociation(subAssoc);
-        }
-    }
-    else
-    {
-        T_DIMSE_Message     msg;
-        T_ASC_PresentationContextID presID;
-
-        if (!ASC_dataWaiting(*subAssoc, 0)) /* just in case */
-            return;
-
-        OFCondition cond = DIMSE_receiveCommand(*subAssoc, DIMSE_BLOCKING, 30, &presID,
-                &msg, NULL);
-
-        if (cond == EC_Normal) {
-          switch (msg.CommandField)
-          {
-            case DIMSE_C_STORE_RQ:
-              cond = storeSCP(*subAssoc, &msg, presID);
-              break;
-            case DIMSE_C_ECHO_RQ:
-              break;
-            default:
-              cond = DIMSE_BADCOMMANDTYPE;
-              std::cout << "cannot handle command: 0x"
-                   << STD_NAMESPACE hex << OFstatic_cast(unsigned, msg.CommandField) << std::endl;
-              break;
-          }
-        }
-        /* clean up on association termination */
-        if (cond == DUL_PEERREQUESTEDRELEASE)
-        {
-            cond = ASC_acknowledgeRelease(*subAssoc);
-            ASC_dropSCPAssociation(*subAssoc);
-            ASC_destroyAssociation(subAssoc);
-            return;
-        }
-        else if (cond == DUL_PEERABORTEDASSOCIATION)
-        {
-        }
-        else if (cond != EC_Normal)
-        {
-            OFString temp_str;
-            std::cout << "DIMSE failure (aborting sub-association): " << DimseCondition::dump(temp_str, cond) << std::endl;
-            /* some kind of error so abort the association */
-            cond = ASC_abortAssociation(*subAssoc);
-        }
-
-        if (cond != EC_Normal)
-        {
-            ASC_dropAssociation(*subAssoc);
-            ASC_destroyAssociation(subAssoc);
-        }
-    }
-}
 
 BOOST_FIXTURE_TEST_CASE(TEST_OK_02, TestDataOK02)
 {
