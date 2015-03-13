d80fbbea942b7c9b3315d8ac232b3f383cd6e244
Romain Lahaxe <rom.lahaxe@gmail.com>
2015-03-13 16:10:58 +0100

    tests/code: use movescu from DCMTK to test the PACS

--- tests/code/SCP/MoveSCP.cpp
+++ tests/code/SCP/MoveSCP.cpp
@@ -16,163 +16,22 @@
 #include <dcmtk/config/osconfig.h>
 #include <dcmtk/dcmdata/dctk.h>
 
+#include <qt4/Qt/qstring.h>
+#include <qt4/Qt/qstringlist.h>
+#include <qt4/Qt/qprocess.h>
+
 #include "core/ExceptionPACS.h"
 #include "SCP/MoveSCP.h"
 
 /**
  * Pre-conditions:
- *     - we assume that ConfigurationPACS works correctly
- *     - we assume that AuthenticatorNone works correctly
- *     - we assume that DBConnection works correctly
+ *     - we assume that NetworkPACS works correctly
  *
  *     - Following Environment variables should be defined
  *          * DOPAMINE_TEST_LISTENINGPORT
  *          * DOPAMINE_TEST_WRITINGPORT
- *          * DOPAMINE_TEST_CONFIG
+ *          * DOPAMINE_TEST_OUTPUTDIR
  */
-
-/*************************** Tools functions **************************/
-void
-storeSCPCallback(
-    /* in */
-    void *callbackData,
-    T_DIMSE_StoreProgress *progress,                /* progress state */
-    T_DIMSE_C_StoreRQ *req,                         /* original store request */
-    char *imageFileName, DcmDataset **imageDataSet, /* being received into */
-    /* out */
-    T_DIMSE_C_StoreRSP *rsp,                        /* final store response */
-    DcmDataset **statusDetail)
-{
-    if (progress->state == DIMSE_StoreEnd)
-    {
-        *statusDetail = NULL;    /* no status detail */
-    }
-}
-
-OFCondition
-storeSCP(T_ASC_Association *assoc,
-         T_DIMSE_Message *msg,
-         T_ASC_PresentationContextID presID)
-{
-    T_DIMSE_C_StoreRQ *req = &msg->msg.CStoreRQ;
-
-    DcmFileFormat dcmff;
-    DcmDataset *dset = dcmff.getDataset();
-
-    OFCondition cond = DIMSE_storeProvider(assoc, presID, req, NULL, 0,
-                                           &dset, storeSCPCallback, NULL,
-                                           DIMSE_BLOCKING, 30);
-    if (cond.bad())
-    {
-        std::stringstream stream;
-        stream << "Cannot store dataset: " << cond.text();
-        throw dopamine::ExceptionPACS(stream.str());
-    }
-
-    return cond;
-}
-
-void
-subOpCallback(void * /*subOpCallbackData*/ ,
-              T_ASC_Network *aNet, T_ASC_Association **subAssoc)
-{
-    if (aNet == NULL) return;
-
-    if (*subAssoc == NULL)
-    {
-        const char* knownAbstractSyntaxes[] =
-        {
-            UID_VerificationSOPClass
-        };
-
-        OFCondition cond = ASC_receiveAssociation(aNet, subAssoc, ASC_DEFAULTMAXPDU);
-        if (cond.good())
-        {
-            const char* transferSyntaxes[] = { NULL, NULL, NULL, NULL };
-            int numTransferSyntaxes;
-            if (gLocalByteOrder == EBO_LittleEndian)  /* defined in dcxfer.h */
-            {
-                transferSyntaxes[0] = UID_LittleEndianExplicitTransferSyntax;
-                transferSyntaxes[1] = UID_BigEndianExplicitTransferSyntax;
-            }
-            else
-            {
-                transferSyntaxes[0] = UID_BigEndianExplicitTransferSyntax;
-                transferSyntaxes[1] = UID_LittleEndianExplicitTransferSyntax;
-            }
-            transferSyntaxes[2] = UID_LittleEndianImplicitTransferSyntax;
-            numTransferSyntaxes = 3;
-
-            /* accept the Verification SOP Class if presented */
-            cond = ASC_acceptContextsWithPreferredTransferSyntaxes(
-                        (*subAssoc)->params, knownAbstractSyntaxes,
-                        DIM_OF(knownAbstractSyntaxes),
-                        transferSyntaxes, numTransferSyntaxes);
-
-            if (cond.good())
-            {
-                /* the array of Storage SOP Class UIDs comes from dcuid.h */
-                cond = ASC_acceptContextsWithPreferredTransferSyntaxes(
-                            (*subAssoc)->params, dcmAllStorageSOPClassUIDs,
-                            numberOfAllDcmStorageSOPClassUIDs,
-                            transferSyntaxes, numTransferSyntaxes);
-            }
-        }
-        if (cond.good()) cond = ASC_acknowledgeAssociation(*subAssoc);
-        if (cond.bad())
-        {
-            cond = ASC_dropAssociation(*subAssoc);
-            cond = ASC_destroyAssociation(subAssoc);
-        }
-    }
-    else
-    {
-        T_DIMSE_Message     msg;
-        T_ASC_PresentationContextID presID;
-
-        if (!ASC_dataWaiting(*subAssoc, 0)) /* just in case */
-            return;
-
-        OFCondition cond = DIMSE_receiveCommand(*subAssoc, DIMSE_BLOCKING, 30,
-                                                &presID, &msg, NULL);
-        if (cond == EC_Normal)
-        {
-            switch (msg.CommandField)
-            {
-            case DIMSE_C_STORE_RQ:
-                cond = storeSCP(*subAssoc, &msg, presID);
-                break;
-            default:
-                cond = DIMSE_BADCOMMANDTYPE;
-                break;
-            }
-        }
-        /* clean up on association termination */
-        if (cond == DUL_PEERREQUESTEDRELEASE)
-        {
-            cond = ASC_acknowledgeRelease(*subAssoc);
-            cond = ASC_dropSCPAssociation(*subAssoc);
-            cond = ASC_destroyAssociation(subAssoc);
-            return;
-        }
-        else if (cond == DUL_PEERABORTEDASSOCIATION)
-        {
-        }
-        else if (cond != EC_Normal)
-        {
-            std::stringstream stream;
-            stream << "DIMSE failure (aborting sub-association): " << cond.text();
-            cond = ASC_abortAssociation(*subAssoc);
-            throw dopamine::ExceptionPACS(stream.str());
-        }
-
-        if (cond != EC_Normal)
-        {
-            cond = ASC_dropAssociation(*subAssoc);
-            cond = ASC_destroyAssociation(subAssoc);
-        }
-    }
-}
 
 /*************************** TEST OK 01 *******************************/
 /**
@@ -194,110 +53,51 @@ BOOST_AUTO_TEST_CASE(TEST_OK_01)
  */
 BOOST_AUTO_TEST_CASE(TEST_OK_02)
 {
-    DcmDataset dataset;
-    dataset.putAndInsertOFStringArray(DCM_PatientName, "Doe^Jane");
-    dataset.putAndInsertOFStringArray(DCM_QueryRetrieveLevel, "PATIENT");
-
-    std::string writingport(getenv("DOPAMINE_TEST_WRITINGPORT"));
-
-    T_ASC_Network * networkSCU;
-    OFCondition condition = ASC_initializeNetwork(NET_ACCEPTORREQUESTOR,
-                                                  atoi(writingport.c_str()),
-                                                  30, &networkSCU);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    T_ASC_Parameters * params;
-    condition = ASC_createAssociationParameters(&params, ASC_DEFAULTMAXPDU);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    condition = ASC_setAPTitles(params, "LOCAL", "REMOTE", NULL);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    std::string localhost(128, '\0');
-    gethostname(&localhost[0], localhost.size()-1);
-
     std::string listeningport(getenv("DOPAMINE_TEST_LISTENINGPORT"));
+    std::string writingport(getenv("DOPAMINE_TEST_WRITINGPORT"));
+    std::string outputdir(getenv("DOPAMINE_TEST_OUTPUTDIR"));
 
-    std::stringstream addressport;
-    addressport << "localhost:" << listeningport;
-    condition = ASC_setPresentationAddresses(params, localhost.c_str(),
-                                             addressport.str().c_str());
-    BOOST_CHECK_EQUAL(condition.good(), true);
+    std::stringstream com_string;
+    com_string << "movescu -aet LOCAL -aec REMOTE -aem LOCAL "
+               << "+P " << writingport
+               << " -P -k 0010,0010=\"Doe^Jane\" -k 0008,0052=\"PATIENT\" -od "
+               << outputdir << " localhost " << listeningport;
+    QString command_(com_string.str().c_str());
 
-    typedef std::pair<std::string, std::vector<std::string> > PresentationContext;
-    std::vector<PresentationContext> presentation_contexts;
-    std::vector<std::string> tempvect = { UID_LittleEndianImplicitTransferSyntax,
-                                          UID_LittleEndianExplicitTransferSyntax,
-                                          UID_BigEndianExplicitTransferSyntax};
-    presentation_contexts.push_back(std::make_pair(UID_MRImageStorage,
-                                                   tempvect));
+    QProcess *myProcess = new QProcess();
+    myProcess->start(command_);
+    myProcess->waitForFinished(5000);
 
-    unsigned int context_id = 1;
-    for(auto const & context: presentation_contexts)
+    // Check results
+    BOOST_CHECK_EQUAL(myProcess->exitCode(), 0);
+    BOOST_CHECK_EQUAL(myProcess->exitStatus(), QProcess::NormalExit);
+    BOOST_CHECK_EQUAL(myProcess->readAllStandardOutput().length(), 0);
+    BOOST_CHECK_EQUAL(myProcess->readAllStandardError().length(), 0);
+
+    bool filecreate = false;
+    // Check dataset
+    typedef boost::filesystem::directory_iterator Iterator;
+    for(Iterator it(outputdir); it != Iterator(); ++it)
     {
-        char const ** transfer_syntaxes = new char const *[context.second.size()];
-        for(std::size_t i = 0; i < context.second.size(); ++i)
+        if(! boost::filesystem::is_directory( (*it) ) )
         {
-            transfer_syntaxes[i] = context.second[i].c_str();
+            DcmFileFormat fileformat;
+            OFCondition condition = fileformat.loadFile((*it).path().c_str());
+            DcmDataset * dataset = fileformat.getDataset();
+
+            BOOST_CHECK_EQUAL(condition == EC_Normal, true);
+
+            OFString patient_name;
+            condition = dataset->findAndGetOFStringArray(DCM_PatientName, patient_name);
+            BOOST_CHECK_EQUAL(condition == EC_Normal, true);
+            BOOST_CHECK_EQUAL(std::string(patient_name.c_str()), "Doe^Jane");
+
+            filecreate = true;
+
+            boost::filesystem::remove((*it).path());
         }
-
-        condition = ASC_addPresentationContext(params, context_id,
-                                               context.first.c_str(),
-                                               transfer_syntaxes,
-                                               context.second.size());
-        BOOST_CHECK_EQUAL(condition.good(), true);
-
-        context_id += 2;
     }
-
-    T_ASC_Association * association;
-    condition = ASC_requestAssociation(networkSCU, params, &association);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    T_ASC_PresentationContextID const presentation_id =
-        ASC_findAcceptedPresentationContextID(association,
-                                              UID_MRImageStorage);
-    BOOST_CHECK_EQUAL(presentation_id != 0, true);
-
-    T_ASC_PresentationContext pc;
-    condition = ASC_findAcceptedPresentationContext(association->params,
-                                                    presentation_id, &pc);
-    BOOST_CHECK_EQUAL(presentation_id != 0, true);
-
-    DIC_US const message_id = association->nextMsgID++;
-
-    T_DIMSE_C_MoveRQ * request = new T_DIMSE_C_MoveRQ();
-    memset(request, 0, sizeof(*request));
-    request->MessageID = message_id;
-    strcpy(request->AffectedSOPClassUID, UID_MRImageStorage);
-
-    request->DataSetType = DIMSE_DATASET_PRESENT;
-    request->Priority = DIMSE_PRIORITY_MEDIUM;
-
-    condition = ASC_getAPTitles(association->params, request->MoveDestination,
-                NULL, NULL);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    T_DIMSE_C_MoveRSP response;
-    DcmDataset *detail = NULL;
-    DcmDataset *rspIds = NULL;
-
-    condition = DIMSE_moveUser(association, presentation_id, request, &dataset,
-                               NULL, NULL, DIMSE_BLOCKING, 30, networkSCU,
-                               subOpCallback, NULL, &response, &detail,
-                               &rspIds, OFTrue);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-    BOOST_CHECK_EQUAL(detail == NULL, true);
-    BOOST_CHECK_EQUAL(response.DimseStatus, STATUS_Success);
-
-    condition = ASC_releaseAssociation(association);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    condition = ASC_destroyAssociation(&association);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    condition = ASC_dropNetwork(&networkSCU);
-    BOOST_CHECK_EQUAL(condition.good(), true);
+    BOOST_CHECK_EQUAL(filecreate, true);
 }
 
 /*************************** TEST KO 01 *******************************/
@@ -306,108 +106,24 @@ BOOST_AUTO_TEST_CASE(TEST_OK_02)
  */
 BOOST_AUTO_TEST_CASE(TEST_KO_01)
 {
-    DcmDataset dataset;
-    dataset.putAndInsertOFStringArray(DCM_PatientName, "Doe^Jane");
-
-    std::string writingport(getenv("DOPAMINE_TEST_WRITINGPORT"));
-
-    T_ASC_Network * networkSCU;
-    OFCondition condition = ASC_initializeNetwork(NET_ACCEPTORREQUESTOR,
-                                                  atoi(writingport.c_str()),
-                                                  30, &networkSCU);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    T_ASC_Parameters * params;
-    condition = ASC_createAssociationParameters(&params, ASC_DEFAULTMAXPDU);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    condition = ASC_setAPTitles(params, "LOCAL", "REMOTE", NULL);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    std::string localhost(128, '\0');
-    gethostname(&localhost[0], localhost.size()-1);
-
     std::string listeningport(getenv("DOPAMINE_TEST_LISTENINGPORT"));
+    std::string writingport(getenv("DOPAMINE_TEST_WRITINGPORT"));
+    std::string outputdir(getenv("DOPAMINE_TEST_OUTPUTDIR"));
 
-    std::stringstream addressport;
-    addressport << "localhost:" << listeningport;
-    condition = ASC_setPresentationAddresses(params, localhost.c_str(),
-                                             addressport.str().c_str());
-    BOOST_CHECK_EQUAL(condition.good(), true);
+    std::stringstream com_string;
+    com_string << "movescu -aet LOCAL -aec REMOTE -aem LOCAL"
+               << "+P " << writingport
+               << " -P -k 0010,0010=\"Doe^Jane\" -od "
+               << outputdir << " localhost " << listeningport;
+    QString command_(com_string.str().c_str());
 
-    typedef std::pair<std::string, std::vector<std::string> > PresentationContext;
-    std::vector<PresentationContext> presentation_contexts;
-    std::vector<std::string> tempvect = { UID_LittleEndianImplicitTransferSyntax,
-                                          UID_LittleEndianExplicitTransferSyntax,
-                                          UID_BigEndianExplicitTransferSyntax};
-    presentation_contexts.push_back(std::make_pair(UID_MRImageStorage,
-                                                   tempvect));
+    QProcess *myProcess = new QProcess();
+    myProcess->start(command_);
+    myProcess->waitForFinished(5000);
 
-    unsigned int context_id = 1;
-    for(auto const & context: presentation_contexts)
-    {
-        char const ** transfer_syntaxes = new char const *[context.second.size()];
-        for(std::size_t i = 0; i < context.second.size(); ++i)
-        {
-            transfer_syntaxes[i] = context.second[i].c_str();
-        }
-
-        condition = ASC_addPresentationContext(params, context_id,
-                                               context.first.c_str(),
-                                               transfer_syntaxes,
-                                               context.second.size());
-        BOOST_CHECK_EQUAL(condition.good(), true);
-
-        context_id += 2;
-    }
-
-    T_ASC_Association * association;
-    condition = ASC_requestAssociation(networkSCU, params, &association);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    T_ASC_PresentationContextID const presentation_id =
-        ASC_findAcceptedPresentationContextID(association,
-                                              UID_MRImageStorage);
-    BOOST_CHECK_EQUAL(presentation_id != 0, true);
-
-    T_ASC_PresentationContext pc;
-    condition = ASC_findAcceptedPresentationContext(association->params,
-                                                    presentation_id, &pc);
-    BOOST_CHECK_EQUAL(presentation_id != 0, true);
-
-    DIC_US const message_id = association->nextMsgID++;
-
-    T_DIMSE_C_MoveRQ * request = new T_DIMSE_C_MoveRQ();
-    memset(request, 0, sizeof(*request));
-    request->MessageID = message_id;
-    strcpy(request->AffectedSOPClassUID, UID_MRImageStorage);
-
-    request->DataSetType = DIMSE_DATASET_PRESENT;
-    request->Priority = DIMSE_PRIORITY_MEDIUM;
-
-    condition = ASC_getAPTitles(association->params, request->MoveDestination,
-                NULL, NULL);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    T_DIMSE_C_MoveRSP response;
-    DcmDataset *detail = NULL;
-    DcmDataset *rspIds = NULL;
-
-    condition = DIMSE_moveUser(association, presentation_id, request, &dataset,
-                               NULL, NULL, DIMSE_BLOCKING, 30, networkSCU,
-                               subOpCallback, NULL, &response, &detail,
-                               &rspIds, OFTrue);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-    BOOST_CHECK_EQUAL(response.DimseStatus,
-                      STATUS_MOVE_Failed_IdentifierDoesNotMatchSOPClass);
-    BOOST_CHECK_EQUAL(detail != NULL, true);
-
-    condition = ASC_releaseAssociation(association);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    condition = ASC_destroyAssociation(&association);
-    BOOST_CHECK_EQUAL(condition.good(), true);
-
-    condition = ASC_dropNetwork(&networkSCU);
-    BOOST_CHECK_EQUAL(condition.good(), true);
+    // Check results
+    BOOST_CHECK_EQUAL(myProcess->exitCode(), 0);
+    BOOST_CHECK_EQUAL(myProcess->exitStatus(), QProcess::NormalExit);
+    BOOST_CHECK_EQUAL(myProcess->readAllStandardOutput().length(), 0);
+    BOOST_CHECK_GT(myProcess->readAllStandardError().length(), 0);
 }
