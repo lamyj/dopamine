641e7ce191955a3a01704afb6871735f8e4d6fc5
Lahaxe <lahaxe@unistra.fr>
2015-03-27 11:25:51 +0100

    tests: modify unit tests for StoreSCP (redmine task #509)

--- tests/code/SCP/test_StoreSCP.py
+++ tests/code/SCP/test_StoreSCP.py
@@ -10,7 +10,7 @@ class TestStoreSCP(TestBase):
     #################################################
     # Nominal test case: Execute Store
     #################################################
-    def test_run_getscu(self):
+    def test_run_storescu(self):
         try:
             johndoe = os.environ["DOPAMINE_TEST_JOHNDOE"]
             
@@ -26,6 +26,64 @@ class TestStoreSCP(TestBase):
             # Check results
             self.assertEqual(out, "")
             self.assertEqual(err, "")
+            
+        except subprocess.CalledProcessError as error:
+            self.assertEqual(error.returncode, 0)
+    
+    #################################################
+    # Error test case: Not Allowed
+    #################################################
+    def test_store_not_allowed(self):
+        # Remove authorization from database
+        thread_spec = subprocess.Popen(self._create_specific_auth, shell=True)
+        thread_spec.wait()
+        
+        try:
+            johndoe = os.environ["DOPAMINE_TEST_JOHNDOE2"]
+            
+            # Send a Get request
+            subproc = subprocess.Popen(["storescu", "-aet", "LOCAL", "-aec", 
+                                        "REMOTE", "localhost", self._dopamine_port,
+                                        johndoe],
+                                        stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            
+            # Read the output
+            out, err = subproc.communicate()
+            
+            # Check results
+            self.assertEqual(out, "")
+            #self.assertRegexpMatches(err, "Refused: OutOfResources")
+            self.assertRegexpMatches(err, "\(0000,0901\) AT \(ffff,ffff\)")
+            self.assertRegexpMatches(err, "\(0000,0902\) LO \[User not allowed to perform STORE\]")
+            
+        except subprocess.CalledProcessError as error:
+            self.assertEqual(error.returncode, 0)
+        
+    #################################################
+    # Error test case: No Authentication
+    #################################################
+    def test_store_not_authenticate(self):
+        # Remove authorization from database
+        thread_remove = subprocess.Popen(self._remove_authorization, shell=True)
+        thread_remove.wait()
+        
+        try:
+            johndoe = os.environ["DOPAMINE_TEST_JOHNDOE2"]
+            
+            # Send a Get request
+            subproc = subprocess.Popen(["storescu", "-aet", "LOCAL", "-aec", 
+                                        "REMOTE", "localhost", self._dopamine_port,
+                                        johndoe],
+                                        stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            
+            # Read the output
+            out, err = subproc.communicate()
+            
+            # Check results
+            self.assertEqual(out, "")
+            #self.assertRegexpMatches(err, "Refused: OutOfResources")
+            self.assertRegexpMatches(err, "\(0000,0901\) AT \(ffff,ffff\)")
+            self.assertRegexpMatches(err, "\(0000,0902\) LO \[User not allowed to perform STORE\]")
             
         except subprocess.CalledProcessError as error:
             self.assertEqual(error.returncode, 0)
