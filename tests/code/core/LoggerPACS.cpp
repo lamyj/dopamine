75cd1297886ae3f29cfc3ebe80009ad911d57db4
Julien Lamy <lamy@unistra.fr>
2016-06-28 17:11:14 +0200

    Remove test based on uninitialized data

--- tests/code/core/LoggerPACS.cpp
+++ tests/code/core/LoggerPACS.cpp
@@ -6,7 +6,7 @@
  * for details.
  ************************************************************************/
 
-#define BOOST_TEST_MODULE ModuleLoggerPACS
+#define BOOST_TEST_MODULE LoggerPACS
 #include <boost/test/unit_test.hpp>
 
 #include "core/LoggerPACS.h"
@@ -29,26 +29,6 @@ struct TestDataLogger
     }
 };
 
-/******************************* TEST Nominal **********************************/
-/**
- * Nominal test case: Logger Not Initialize
- *                    This test should be done in first.
- */
-BOOST_FIXTURE_TEST_CASE(No_Initialization, TestDataLogger)
-{
-    // Test
-    dopamine::logger_error() << " test ";
-    dopamine::logger_warning() << " test ";
-    dopamine::logger_info() << " test ";
-    dopamine::logger_debug() << " test ";
-
-    BOOST_CHECK_EQUAL(stream.str(), "");
-}
-
-/******************************* TEST Nominal **********************************/
-/**
- * Nominal test case: InitializeLogger (ERROR)
- */
 BOOST_FIXTURE_TEST_CASE(InitializeLogger_Error, TestDataLogger)
 {
     // Initialize logger
@@ -68,10 +48,6 @@ BOOST_FIXTURE_TEST_CASE(InitializeLogger_Error, TestDataLogger)
     BOOST_CHECK(stream.str().find("DEBUG") == std::string::npos);
 }
 
-/******************************* TEST Nominal **********************************/
-/**
- * Nominal test case: InitializeLogger (WARNING)
- */
 BOOST_FIXTURE_TEST_CASE(InitializeLogger_Warning, TestDataLogger)
 {
     // Initialize logger
@@ -91,10 +67,6 @@ BOOST_FIXTURE_TEST_CASE(InitializeLogger_Warning, TestDataLogger)
     BOOST_CHECK(stream.str().find("DEBUG") == std::string::npos);
 }
 
-/******************************* TEST Nominal **********************************/
-/**
- * Nominal test case: InitializeLogger (INFO)
- */
 BOOST_FIXTURE_TEST_CASE(InitializeLogger_Info, TestDataLogger)
 {
     // Initialize logger
@@ -114,10 +86,6 @@ BOOST_FIXTURE_TEST_CASE(InitializeLogger_Info, TestDataLogger)
     BOOST_CHECK(stream.str().find("DEBUG") == std::string::npos);
 }
 
-/******************************* TEST Nominal **********************************/
-/**
- * Nominal test case: InitializeLogger (DEBUG)
- */
 BOOST_FIXTURE_TEST_CASE(InitializeLogger_Debug, TestDataLogger)
 {
     // Initialize logger
@@ -137,10 +105,6 @@ BOOST_FIXTURE_TEST_CASE(InitializeLogger_Debug, TestDataLogger)
     BOOST_CHECK(stream.str().find("DEBUG") != std::string::npos);
 }
 
-/******************************* TEST Nominal **********************************/
-/**
- * Nominal test case: InitializeLogger (DEFAULT)
- */
 BOOST_FIXTURE_TEST_CASE(InitializeLogger_Default, TestDataLogger)
 {
     // Initialize logger
